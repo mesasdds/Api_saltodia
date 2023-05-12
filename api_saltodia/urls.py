98f2a6b4c86d9e8f37fdb1211a4e82b0a2b8d201
mesasdds <mesasdds@gmail.com>
2023-05-12 09:13:08 -0300

    correção no envio de imagens, fix

--- api_saltodia/urls.py
+++ api_saltodia/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from saltodados import views
 from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 
 router = routers.DefaultRouter()
 
 router.register(r'ArtigoPrincipal', views.ArtigoPrincipalView, 'ArtigoPrincipal')
 router.register(r'ArtigoSecundario', views.ArtigoSecundarioView, 'ArtigoSecundario')
-router.register(r'ArtigoTerceiro', views.ArtigoSecundarioView, 'ArtigoTerceiro')
+router.register(r'ArtigoTerceiro', views.ArtigoTerceiroView, 'ArtigoTerceiro')
 router.register(r'ArtigosGenericos', views.ArtigosGenericosView, 'ArtigosGenericos')
 router.register(r'ArtigosRecommends', views.ArtigosRecommendsView, 'ArtigosRecommends')
 
@@ -16,4 +19,8 @@ router.register(r'ArtigosRecommends', views.ArtigosRecommendsView, 'ArtigosRecom
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls))
-]
+] 
+ 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
